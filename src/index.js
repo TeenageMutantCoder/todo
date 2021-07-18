8ddc9f11eefef9a1906969ac83a76f773c9bb635
Stevon Wright <stevonisepic@gmail.com>
2021-07-18 16:03:18 -0400

    Revert "Fixed mobile drag scroll bug (I think)"
    This reverts commit c9dc338db3799f6e20e3acadbced0146268092ea.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import {polyfill} from "mobile-drag-drop"; // Allows drag-and-drop code to work on mobile browsers https://github.com/timruffles/mobile-drag-drop
 
+import {scrollBehaviourDragImageTranslateOverride} from "mobile-drag-drop/scroll-behaviour";
 polyfill({
+    // If I understand this correctly, allows scrolling while dragging is close to the end of the screen
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride,
     // Sets drag delay to allow scrolling instead
     holdToDrag: 150
 });
